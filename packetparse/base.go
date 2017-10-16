df904c22c3bfe66eda6885a31bc36e42673f943e
kexirong <kexirong@qkagame.com>
2017-10-16 10:55:50 +0800

    .

--- packetparse/base.go
+++ packetparse/base.go
@@ -1,64 +1,57 @@
 package packetparse
 
-import(
-    "reflect"
-    "fmt"
+import (
+	"reflect"
 )
 
-
 /*
-    | 0 | 1 | 2 | 3 |
-    +-------+-------+
-    |  type |length |
-    +-------+-------+
-    | **** data***  |
-    |   *********   | 
-    +---------------+ 
+   | 0 | 1 | 2 | 3 |
+   +-------+-------+
+   |  type |length |
+   +-------+-------+
+   | **** data***  |
+   |   *********   |
+   +---------------+
 */
 
-type  Packet struct {
-    HostName  string        `json:"hostname"`  //ops201
-    TimeStamp float64       `json:"timestamp"` //23123131.123131
-    Plugin    string        `json:"plugin"`    // cpu
-    Instance  string        `json:"instance"`  // 0,1,2,3 (0_idle,0_user,)
-    Type      string        `json:"type"`      //percent,counter,gauge,derive
-    Value     []float64     `json:"value"`
-    VlTags    string        `json:"vltags"`    // "idle:user:system"
-    Message   string       ` json:"message"`
+type Packet struct {
+	HostName  string    `json:"hostname"`  //ops201
+	TimeStamp float64   `json:"timestamp"` //23123131.123131
+	Plugin    string    `json:"plugin"`    // cpu
+	Instance  string    `json:"instance"`  // 0,1,2,3 (0_idle,0_user,)
+	Type      string    `json:"type"`      //percent,counter,gauge,derive
+	Value     []float64 `json:"value"`
+	VlTags    string    `json:"vltags"` // "idle:user:system"
+	Message   string    ` json:"message"`
 }
 
-var packMap  = map[string]uint16{
-    "hostname" : 0x0000,
-    "timestamp": 0x0001,
-    "plugin"   : 0x0002,
-    "instance" : 0x0003,
-    "type"     : 0x0004,
-    "value"    : 0x0005,
-    "vltags"   : 0x0006,
-    "message"  : 0x0007,
+var packMap = map[string]uint16{
+	"hostname":  0x0000,
+	"timestamp": 0x0001,
+	"plugin":    0x0002,
+	"instance":  0x0003,
+	"type":      0x0004,
+	"value":     0x0005,
+	"vltags":    0x0006,
+	"message":   0x0007,
 }
 
-var typesMap   map[string]string  //[name]tyoe
-var parseMap   map[uint16]string   //[id]name
+var typesMap map[string]string //[name]tyoe
+var parseMap map[uint16]string //[id]name
 
+func init() {
+	parseMap = make(map[uint16]string)
+	typesMap = make(map[string]string)
 
+	for key, vl := range packMap {
+		parseMap[vl] = key
+	}
 
-func init(){
-    parseMap = make(map[uint16]string)
-    typesMap = make(map[string]string)
-    
-    for key,vl := range packMap{
-        parseMap[vl]=key
-    }
-    
-    var packet  Packet
-    t := reflect.TypeOf(packet)
-    v := reflect.ValueOf(packet)
-    
-    for k := 0; k < t.NumField(); k++ {
-        typesMap[ t.Field(k).Tag.Get("json")] = v.Field(k).Kind().String()
-    }
+	var packet Packet
+	t := reflect.TypeOf(packet)
+	v := reflect.ValueOf(packet)
+
+	for k := 0; k < t.NumField(); k++ {
+		typesMap[t.Field(k).Tag.Get("json")] = v.Field(k).Kind().String()
+	}
 }
-
-
-
--- packetparse/packetparse.go
+++ packetparse/packetparse.go
@@ -1,264 +1,236 @@
 package packetparse
 
-import(
-    "fmt"
-    "reflect"
-    "bytes"
-    "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
 
-    "unsafe"
-
+	"unsafe"
 )
 
-
-
-
-var Network  network
+var Network network
 
 type network struct{}
 
 //network(=big-Endian)
 func (network) Float64ToBytes(f float64) []byte {
-    bits :=  *(*uint64)(unsafe.Pointer(&f))
-    b := make([]byte, 8)
-    b[0] = byte(bits >> 56)
-   	b[1] = byte(bits >> 48)
-   	b[2] = byte(bits >> 40)
-   	b[3] = byte(bits >> 32)
-   	b[4] = byte(bits >> 24)
-   	b[5] = byte(bits >> 16)
-   	b[6] = byte(bits >> 8)
-   	b[7] = byte(bits)
+	bits := *(*uint64)(unsafe.Pointer(&f))
+	b := make([]byte, 8)
+	b[0] = byte(bits >> 56)
+	b[1] = byte(bits >> 48)
+	b[2] = byte(bits >> 40)
+	b[3] = byte(bits >> 32)
+	b[4] = byte(bits >> 24)
+	b[5] = byte(bits >> 16)
+	b[6] = byte(bits >> 8)
+	b[7] = byte(bits)
 
-    return b
+	return b
 }
 
 //native(=little-Endian)
-func (network) BytesToFloat64(b []byte ) float64 {
+func (network) BytesToFloat64(b []byte) float64 {
 
-      bits :=  uint64(b[0])<<56| uint64(b[1])<<48|
-               uint64(b[2])<<40| uint64(b[3])<<32| 
-               uint64(b[4])<<24| uint64(b[5])<<16|
-               uint64(b[6])<<8 |uint64(b[7]) 
-            
-    return *(*float64)(unsafe.Pointer(&bits))
+	bits := uint64(b[0])<<56 | uint64(b[1])<<48 |
+		uint64(b[2])<<40 | uint64(b[3])<<32 |
+		uint64(b[4])<<24 | uint64(b[5])<<16 |
+		uint64(b[6])<<8 | uint64(b[7])
+
+	return *(*float64)(unsafe.Pointer(&bits))
 }
-
 
 //network(=big-Endian)
-func (network) Uint16ToBytes(u uint16 ) []byte {
-    b := make([]byte, 2)
+func (network) Uint16ToBytes(u uint16) []byte {
+	b := make([]byte, 2)
 
-    b[0] = byte(u >> 8)
-   	b[1] = byte(u)
+	b[0] = byte(u >> 8)
+	b[1] = byte(u)
 
-    return  b
+	return b
 
 }
-
 
 func (network) BytesToUint16(b []byte) uint16 {
 
-    return uint16(b[1]) | uint16(b[0])<<8
+	return uint16(b[1]) | uint16(b[0])<<8
 
 }
 
+func bufwrite(buf io.Writer, data interface{}) error {
+	var err error
+	var leng uint16
 
+	switch data.(type) {
 
+	case string:
+		leng = uint16(len(data.(string)))
+		_, err = buf.Write(Network.Uint16ToBytes(leng))
 
+		if err != nil {
+			return err
+		}
 
-func bufwrite(buf io.Writer,data interface{}) error{
-    var err error
-    var leng uint16
+		_, err = buf.Write([]byte(data.(string)))
 
-    switch data.(type){
+		if err != nil {
+			return err
+		}
 
-        case string:
-            leng = uint16(len(data.(string)))
-            _, err = buf.Write(Network.Uint16ToBytes(leng))
+	case float64:
+		leng = 8
+		_, err = buf.Write(Network.Uint16ToBytes(leng))
 
-            if err != nil {
-                return err
-            }
+		if err != nil {
+			return err
+		}
 
-            _, err = buf.Write([]byte(data.(string)))
+		_, err = buf.Write(Network.Float64ToBytes(data.(float64)))
 
-            if err != nil {
-                return err
-            }
+		if err != nil {
+			return err
+		}
 
-        case float64:
-            leng = 8
-            _, err = buf.Write(Network.Uint16ToBytes(leng))
+	case []float64:
+		leng = uint16(8 * len(data.([]float64)))
+		_, err = buf.Write(Network.Uint16ToBytes(leng))
 
-            if err != nil {
-                return err
-            }
+		if err != nil {
+			return err
+		}
 
-            _, err = buf.Write(Network.Float64ToBytes(data.(float64)))
+		for _, value := range data.([]float64) {
+			_, err = buf.Write(Network.Float64ToBytes((value)))
+		}
+	}
 
-            if err != nil {
-                return err
-            }
+	if err != nil {
+		return err
+	}
 
-
-        case []float64:
-            leng = uint16(8*len(data.([]float64)))
-            _, err = buf.Write(Network.Uint16ToBytes(leng))
-
-            if err != nil {
-                return err
-            }
-
-            for _, value := range data.([]float64) {
-                _, err = buf.Write(Network.Float64ToBytes((value)))
-            }
-    }
-
-    if err != nil {
-        return err
-    }
-
-    return nil
+	return nil
 
 }
 
+func Package(packet Packet) ([]byte, error) {
+	var err error
+	buf := new(bytes.Buffer)
 
-func Package(packet Packet) ([]byte ,error){
-    var err error
-    buf := new(bytes.Buffer)
+	value := reflect.ValueOf(packet)
+	num := value.NumField()
 
-    value := reflect.ValueOf(packet)
-    num := value.NumField()
-    
-    /*if value.Field(num-1) == "" {
-        num = num -1
-    }*/
-    
-    for i := 0; i < num; i++ {
-        v := value.Field(i)
- 
-        //content
+	/*if value.Field(num-1) == "" {
+	    num = num -1
+	}*/
 
-        switch v.Interface() {
+	for i := 0; i < num; i++ {
+		v := value.Field(i)
 
-            case "", float64(0) ,nil:
-                
-                if !(uint16(i) == packMap["vltags"] || uint16(i) == packMap["message"]) {
-                    return nil,fmt.Errorf("Field: %d (%d) is empty",i ,packMap["message"] )
-                }
+		//content
 
-            default:
-                _, err = buf.Write(Network.Uint16ToBytes(uint16(i)))
+		switch v.Interface() {
 
-                if err != nil {
-                    return nil,err
-                }
+		case "", float64(0), nil:
 
-                err = bufwrite( buf, v.Interface())
+			if !(uint16(i) == packMap["vltags"] || uint16(i) == packMap["message"]) {
+				return nil, fmt.Errorf("Field: %d (%d) is empty", i, packMap["message"])
+			}
 
-                if err != nil {
-                    return nil,err
-                }
-        }
-    }
-    return buf.Bytes(),nil
+		default:
+			_, err = buf.Write(Network.Uint16ToBytes(uint16(i)))
 
-}
+			if err != nil {
+				return nil, err
+			}
 
+			err = bufwrite(buf, v.Interface())
 
-
-func Parse(b []byte) (Packet,error){
-    var err error
-    var packet Packet
-    var t, l uint16
-    var str string
-    var f float64
-    var sf []float64
-    n := 0
-    
-    head := make([]byte,4)
-    
-    
-    bufs := bytes.NewReader(b)
-    for ;;n++{
-        _,err = bufs.Read(head)
-        
-        if  err == io.EOF {
-            break 
-        }
-        t = Network.BytesToUint16(head[0:2])
-        l = Network.BytesToUint16(head[2:4])
-        
-        buf := make([]byte,l)
-        
-        _, err = bufs.Read(buf)
-        if err != nil {
-            return packet, err
-        }
-        
-        field := parseMap[t]
-        kind  := typesMap[field]
-        
-        switch kind {
-            case "string" :
-                str= string(buf)
-                
-            case "float64" :
-                f = Network.BytesToFloat64(buf)
-                
-            case "slice":
-                e:=8
-                for i:=0; i< int(l);i+=8{
-                    f = Network.BytesToFloat64(buf[i:i+e])
-                    sf=append(sf,f)
-                }
-                
-            default:
-                return packet,  fmt.Errorf("packet parse type error : n=%d, t=%d", n,t)
-        }
-        
-        switch field {
-            case "hostname" :
-                packet.HostName=str
-            
-            case "timestamp" :
-                packet.TimeStamp=f
-            
-            case "plugin" :
-                packet.Plugin=str
-            
-            case "instance" :
-                packet.Instance=str
-            case "type" : 
-                packet.Type=str
-            
-            case "value" : 
-                packet.Value=sf
-            case "vltags" : 
-                packet.VlTags=str
-            case "message" :
-                packet.Message=str
-            default:
-                return packet,  fmt.Errorf("packet parse field error : n=%d, t=%d", n,t)
-
-        }
-        
-        
-    }
-    
-    
-    return packet,nil
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return buf.Bytes(), nil
 
 }
 
+func Parse(b []byte) (Packet, error) {
+	var err error
+	var packet Packet
+	var t, l uint16
+	var str string
+	var f float64
+	var sf []float64
+	n := 0
 
+	head := make([]byte, 4)
 
+	bufs := bytes.NewReader(b)
+	for ; ; n++ {
+		_, err = bufs.Read(head)
 
+		if err == io.EOF {
+			break
+		}
+		t = Network.BytesToUint16(head[0:2])
+		l = Network.BytesToUint16(head[2:4])
 
+		buf := make([]byte, l)
 
+		_, err = bufs.Read(buf)
+		if err != nil {
+			return packet, err
+		}
 
+		field := parseMap[t]
+		kind := typesMap[field]
 
+		switch kind {
+		case "string":
+			str = string(buf)
 
+		case "float64":
+			f = Network.BytesToFloat64(buf)
 
+		case "slice":
+			e := 8
+			for i := 0; i < int(l); i += 8 {
+				f = Network.BytesToFloat64(buf[i : i+e])
+				sf = append(sf, f)
+			}
 
+		default:
+			return packet, fmt.Errorf("packet parse type error : n=%d, t=%d", n, t)
+		}
+
+		switch field {
+		case "hostname":
+			packet.HostName = str
+
+		case "timestamp":
+			packet.TimeStamp = f
+
+		case "plugin":
+			packet.Plugin = str
+
+		case "instance":
+			packet.Instance = str
+		case "type":
+			packet.Type = str
+
+		case "value":
+			packet.Value = sf
+		case "vltags":
+			packet.VlTags = str
+		case "message":
+			packet.Message = str
+		default:
+			return packet, fmt.Errorf("packet parse field error : n=%d, t=%d", n, t)
+
+		}
+
+	}
+
+	return packet, nil
+
+}
