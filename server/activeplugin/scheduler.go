8226397b95799719dd84b3ab60a0e0aeaa8d2eb8
kexirong <kexirong@outlook.com>
2018-05-20 23:49:23 +0800

    提交

--- server/activeplugin/scheduler.go
+++ server/activeplugin/scheduler.go
@@ -15,127 +15,180 @@ import (
 	"github.com/kexirong/monitor/common/packetparse"
 )
 
-type Plugin interface {
-	Gather() ([]packetparse.TargetPacket, error)
+type Tasker interface {
+	Do() ([]packetparse.TargetPacket, error)
 	AddJob(args ...interface{}) error
 	Name() string
 	DeleteJob(target string) error
 }
 
-//pluginEntry  is  a ring
-type pluginEntry struct {
+//taskList  实现双向环形链表
+type taskList struct {
 	runing       bool
 	nextTime     time.Time
-	interval     time.Duration
-	pNext, pPrev *pluginEntry
-	plugin       Plugin
-}
-
-func (p *pluginEntry) insert(e *pluginEntry) {
-	//	n := p.pNext
-	//	p.pNext = e
-	//	e.pNext = n
-	//	e.pPrev = p
-	//	n.pPrev = e
-	e.pNext = p.pNext
-	p.pNext.pPrev = e
-	p.pNext = e
-	e.pPrev = p
-}
-
-//pop  while len==0  will return  nil
-func (p *pluginEntry) pop() *pluginEntry {
-	next := p
-	if p.len() > 1 {
-		p.pPrev.pNext = p.pNext
-		p.pNext.pPrev = p.pPrev
-	} else {
-		p = nil
-	}
-	return next
-}
-
-func (p *pluginEntry) shift(n int) {
-	cur := p.pop()
-
-	if cur.nextTime.Before(next.nextTime) {
-		return
-	}
-	if r.len < 3 {
-		r.next()
-		return
-	}
-	cur = r.pop()
-	for i := 0; i < r.len-1; i++ {
-		r.next()
-		if cur.nextTime.Before(r.curEntry.nextTime) {
-			r.prev()
-			break
-		}
-	}
-	r.insert(cur)
-	r.curEntry = next
-	return
-}
-
-func (p *pluginEntry) isRuning() bool {
-	return p.runing
-}
-
-func (p *pluginEntry) Interval() time.Duration {
-	return p.interval
-}
-
-func (p *pluginEntry) next() *pluginEntry {
-	return p.pNext
-}
-
-func (p *pluginEntry) perv() *pluginEntry {
-	return p.pPrev
+	invl         time.Duration
+	pNext, pPrev *taskList
+	task         Tasker
 }
 
 /*
-func (p *pluginEntry) init() *pluginEntry {
-	p.pNext = p
-	p.pPrev = p
-	return p
+func (t *taskList) init() *taskList {
+	t.pNext = t
+	t.pPrev = t
+	return t
 }
 */
-func (p *pluginEntry) len() int {
+
+//insert 在t节点的前面插入
+func (t *taskList) insert(node *taskList) *taskList {
+	if t == nil {
+		node.pPrev = node
+		node.pNext = node
+	} else {
+		node.pNext = t
+		node.pPrev = t.pPrev
+		t.pPrev.pNext = node
+		t.pPrev = node
+	}
+	return node
+}
+
+//pop  将下一个节点从taskList中去除，并返回此节点，此方法不存在len为0的调用场景
+func (t *taskList) popNext() *taskList {
+	next := t.pNext
+	if t.len() > 1 {
+		t.pNext = next.pNext
+		t.pNext.pPrev = t
+	} else {
+		t = nil
+	}
+	next.pNext = nil
+	next.pPrev = nil
+	return next
+}
+
+//findPos 找到合适的相对位置,n >= 0
+func (t *taskList) findPos() (n int) {
+	next := t.pNext
+	len := t.len()
+	for i := 0; i < len-1; i++ {
+		if t.nextTime.Before(next.nextTime) {
+			break
+		}
+		next = t.pNext
+		n++
+	}
+	return
+}
+
+//shift 使用findpos返回的值,当前节点移动n个位置插入taskList中
+func (t *taskList) shift(n int) {
+	next := t.pNext
+	cur := t.pPrev.popNext()
+	for i := 0; i < n; i++ {
+		next = next.pNext
+	}
+	next.insert(cur)
+}
+
+func (t *taskList) isRuning() bool {
+	return t.runing
+}
+
+func (t *taskList) interval() time.Duration {
+	return t.invl
+}
+
+func (t *taskList) next() *taskList {
+	return t.pNext
+}
+
+func (t *taskList) perv() *taskList {
+	return t.pPrev
+}
+
+func (t *taskList) name() string {
+	return t.task.Name()
+}
+
+func (t *taskList) len() int {
 	n := 0
-	if p != nil {
+	if t != nil {
 		n = 1
-		for r := p.pNext; r != p; p = p.pNext {
+		for r := t.pNext; r != t; t = t.pNext {
 			n++
 		}
 	}
 	return n
 }
 
-//ActivePlugin   内嵌环形链表，不支持并发操作环形链表
-type ActivePlugin struct {
-	event      chan common.Event //"method:pluginnam[|interval]"
-	result     chan common.Event
-	pluginRing *pluginEntry
-	mutex      *sync.Mutex
+//TaskScheduled   pTask 为双向环形链表，mutex保证安全操作链表
+type TaskScheduled struct {
+	event  chan common.Event //"method:pluginnam[|interval]"
+	result chan common.Event
+	pTask  *taskList
+	mutex  *sync.Mutex
 }
 
-//Initialize  初始化, 等同New
-func Initialize() (*ActivePlugin, error) {
-	ring := new(ActivePlugin)
-	ring.result = make(chan common.Event, 1)
-	ring.event = make(chan common.Event, 1)
-	ring.mutex = new(sync.Mutex)
-	return ring, nil
+//New  retrun *TaskScheduled
+func New() *TaskScheduled {
+	ts := new(TaskScheduled)
+	ts.result = make(chan common.Event, 1)
+	ts.event = make(chan common.Event, 1)
+	ts.mutex = new(sync.Mutex)
+	return ts
+}
+
+//AddTask  为了不重复，添加前都尝试一次删除 ,interval单位s ,timeout 必须大于0，单位s
+func (t *TaskScheduled) AddTask(tl *taskList) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	t.DeleteTask(tl.task.Name())
+	t.pTask.insert(tl)
+	t.pTask = t.nextTask()
+}
+
+//DeleteTask .
+func (t *TaskScheduled) DeleteTask(name string) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	len := t.Len()
+	if t.Len() == 0 {
+		return
+	}
+	cur := t.pTask
+	for i := 0; i < len; i++ {
+		if name != cur.name() {
+			cur = cur.next()
+			continue
+		}
+		if cur == t.pTask {
+			t.pTask = t.pTask.next()
+		}
+		cur = cur.perv()
+		cur.popNext()
+		return
+	}
+}
+
+//nextTask 不使用锁,应在AddTask,do中调用，否则非安全的
+func (t *TaskScheduled) nextTask() *taskList {
+	n := t.pTask.findPos()
+	next := t.pTask.next()
+	if n > 0 {
+		t.pTask.shift(n)
+		return next
+	}
+	return t.pTask
 }
 
 //AddEventAndWaitResult add a event and wait eventDeal return result
-func (r *ActivePlugin) AddEventAndWaitResult(event common.Event) common.Event {
-	r.event <- event
-	return <-r.result
+func (t *TaskScheduled) AddEventAndWaitResult(event common.Event) common.Event {
+	t.event <- event
+	return <-t.result
 }
 
-func (r *ActivePlugin) eventDeal(event common.Event) {
+func (t *TaskScheduled) eventDeal(event common.Event) {
 	/*
 		select {
 		case <-r.result:
@@ -145,7 +198,7 @@ func (r *ActivePlugin) eventDeal(event common.Event) {
 	event.Result = "ok"
 	switch event.Method {
 	case "delete":
-		if err := r.DeleteTask(event.Target); err != nil {
+		if err := t.DeleteTask(event.Target); err != nil {
 			event.Result = err.Error()
 		}
 
@@ -164,31 +217,31 @@ func (r *ActivePlugin) eventDeal(event common.Event) {
 			}
 		}
 
-		if err := r.AddTask(event.Target, invl, timeout); err != nil {
+		if err := t.AddTask(event.Target, invl, timeout); err != nil {
 			event.Result = err.Error()
 		}
 	case "getlist":
-		res := r.foreche()
+		res := t.foreche()
 		event.Result = res
 	default:
 		event.Result = "unknown operation type"
 	}
-	r.result <- event
+	t.result <- event
 	return
 
 }
 
 //WaitAndEventDeal 等待阻塞结束和时间
-func (r *ActivePlugin) WaitAndEventDeal() {
+func (t *TaskScheduled) WaitAndEventDeal() {
 	for {
 		var wait = time.After(3 * time.Second)
-		len := r.Len()
+		len := t.Len()
 		if len != 0 {
 			now := time.Now()
-			if r.pluginRing.nextTime.Before(now) {
+			if t.pTask.nextTime.Before(now) {
 				return
 			}
-			wait = time.After(r.pluginRing.nextTime.Sub(now))
+			wait = time.After(t.pTask.nextTime.Sub(now))
 		}
 
 		for {
@@ -198,87 +251,39 @@ func (r *ActivePlugin) WaitAndEventDeal() {
 					break
 				}
 				return
-			case e := <-r.event:
-				r.eventDeal(e)
+			case e := <-t.event:
+				t.eventDeal(e)
 			}
 		}
 	}
 }
 
 //Scheduler must be  after  initialize true
-func (r *ActivePlugin) Scheduler() ([]byte, error) {
+func (t *TaskScheduled) Scheduler() ([]byte, error) {
 	/*
-		if r.len == 0 {
+		if t.Len() == 0 {
 			return nil, errors.New("pluginEntry is empty")
 		}
 	*/
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	pe := r.pluginRing
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	pe := t.pTask
 
 	pe.nextTime = pe.nextTime.Add(pe.interval)
 
 	if pe.runing {
-		return nil, fmt.Errorf("%sis runing, may interval Too brief", pe.plugin.Name())
+		return nil, fmt.Errorf("%sis runing, may interval Too brief", pe.task.Name())
 	}
 	pe.runing = true
 
-	r.fixOrder()
+	t.fixOrder()
 
-	return pe.plugin.Gather()
+	return pe.task.Gather()
 }
 
-//DeleteTask arg PythonModuleName ,as stop the module
-func (r *ActivePlugin) DeleteTask(name string) error {
-	cur := r.pluginRing
-	len := cur.Len()
-	for i := 0; i < len; i++ {
-		if name == r.pluginRing.plugin.Name() {
-			r.pop()
-			if i != 0 {
-				r.pluginRing = cur
-			}
-			return nil
-		}
-		r.next()
-	}
-	r.pluginRing = cur
-	return errors.New("not exist")
-}
-
-/*
-func (r *ActivePlugin) pop() *pluginEntry {
-	cur := r.curEntry
-	r.next()
-	if r.len > 1 {
-		cur.pPrev.pNext = cur.pNext
-		cur.pNext.pPrev = cur.pPrev
-	}
-	r.len--
-	return cur
-}
-*/
-
-//Len return r.len
-func (r *ActivePlugin) Len() int {
-	return r.pluginRing.len()
-}
-
-//InsertEntry  为了不重复，插入前都尝试一次删除 ,interval单位s ,timeout 必须大于0，单位s
-func (r *ActivePlugin) AddTask(name string, interval int, timeout int) error {
-	r.DeleteTask(name)
-	node, err := r.genEntry(name, interval, timeout)
-	if err != nil {
-		return err
-	}
-
-	if r.Len() > 1 {
-		r.prev()
-	}
-	r.insert(node)
-	r.next()
-	r.fixOrder()
-	return nil
+//Len return t.pTask.len()
+func (t *TaskScheduled) Len() int {
+	return t.pTask.len()
 }
 
 /*
@@ -299,62 +304,56 @@ func (r *ActivePlugin) insert(e *pluginEntry) {
 }
 */
 //调度后进行位置修正
-func (r *ActivePlugin) fixOrder() {
-	select {
-	case <-r.plug:
-	default:
-	}
-	defer func() {
-		r.plug <- struct{}{}
-	}()
-	cur := r.curEntry
+func (t *TaskScheduled) fixOrder() {
+
+	cur := t.pTask
 	next := cur.pNext
 
 	if cur.nextTime.Before(next.nextTime) {
 		return
 	}
-	if r.len < 3 {
-		r.next()
+	if t.pTask.len() < 3 {
+		t.next()
 		return
 	}
-	cur = r.pop()
-	for i := 0; i < r.len-1; i++ {
-		r.next()
-		if cur.nextTime.Before(r.curEntry.nextTime) {
-			r.prev()
+	cur = t.pop()
+	for i := 0; i < t.Len()-1; i++ {
+		t.next()
+		if cur.nextTime.Before(t.pTask.nextTime) {
+			t.prev()
 			break
 		}
 	}
-	r.insert(cur)
-	r.curEntry = next
+	t.insert(cur)
+	t.pTask = next
 	return
 }
 
-func (r *ActivePlugin) next() {
-	r.curEntry = r.curEntry.pNext
+func (t *TaskScheduled) next() {
+	t.pTask = t.pTask.pNext
 
 }
-func (r *ActivePlugin) prev() {
-	r.curEntry = r.curEntry.pPrev
+func (t *TaskScheduled) prev() {
+	t.pTask = t.pTask.pPrev
 
 }
 
-func (r *ActivePlugin) genEntry(name string, interval int, timeout int) (*pluginEntry, error) {
-	var pe pluginEntry
+func (t *TaskScheduled) genEntry(name string, interval int, timeout int) (*taskList, error) {
+	var e taskList
 	if interval < 1 || timeout < 1 {
 		return nil, errors.New("arg false: interval or timeout le 0")
 	}
-	pe.name = r.scriptPath + name
+	e.name = t.scriptPath + name
 	if !common.CheckFileIsExist(pe.name) {
 		return nil, errors.New("the script is not exist")
 	}
-	pe.interval = time.Second * time.Duration(interval)
-	pe.timeout = time.Second * time.Duration(timeout)
-	pe.nextTime = time.Now().Add(pe.interval)
+	e.interval = time.Second * time.Duration(interval)
+	e.timeout = time.Second * time.Duration(timeout)
+	e.nextTime = time.Now().Add(pe.interval)
 
-	return &pe, nil
+	return &e, nil
 }
-func (r *ActivePlugin) foreche() string {
+func (t *TaskScheduled) foreche() string {
 	cur := r.curEntry
 	var ret = `{"name":"%s","interval":"%v","nextime":"%s"}`
 	var plugins []string
@@ -368,7 +367,7 @@ func (r *ActivePlugin) foreche() string {
 	return fmt.Sprintf("[%s]", strings.Join(plugins, ","))
 }
 
-func (r *ActivePlugin) CheckDownloads(url, filename string, check bool) error {
+func (t *TaskScheduled) CheckAndDownloads(url, filename string, check bool) error {
 	if check && common.CheckFileIsExist(r.scriptPath+filename) {
 		return nil
 	}
